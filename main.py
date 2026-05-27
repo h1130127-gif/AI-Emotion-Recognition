7a8d333f52b18054283b1748e8590860b4da0ad8
h1130127-gif <h1130127@stu.wghs.tp.edu.tw>
2026-05-27 14:00:05 +0800

    Add emotion analysis interface using Gradio

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 pip install deepface opencv-python
+import gradio as gr
+def analyze_emotion(user_text):
+    return f"系統收到的文字是：'{user_text}'，準備進行 NLP 分析！"
+demo = gr.Interface(
+    fn=analyze_emotion, 
+    inputs=gr.Textbox(label="請輸入你想說的話（例如：我今天好開心）"), 
+    outputs="text"
+)
+demo.launch()
